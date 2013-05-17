d9a35ee8cf237f9ab57e16cae8d305c18ec6fc8a
Steffen Gebert <st+github@st-g.de>
2013-05-17 21:10:51 +0300

    Update metadata - supports

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ description      "Installs/Configures openvz"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.3"
 
-supports         "debian", "= 6"
+supports         "debian", "~> 6.0"
