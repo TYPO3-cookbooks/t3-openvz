11a47118ad5f2886d9bd7bce3fe673be84b9db68
Steffen Gebert <steffen.gebert@typo3.org>
2013-11-20 01:49:05 +0100

    Version bump to 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,11 @@
+name             "openvz"
 maintainer       "TYPO3 Association"
 maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/Configures openvz"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.5"
+version          "1.0.0"
 
 supports         "debian", ">= 6.0"
 depends          "chef-sugar"
+depends          "ohai"
